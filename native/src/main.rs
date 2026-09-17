f7e321d0c08868d7162e5465f4a55dba1b6f4915
Denis Kontek <deniskontek@securesouls.com>
2026-09-17 17:26:00 +0200

    refactor(native): switch renderer from Blitz to system webview
    - drop dioxus-native, use dioxus/desktop (wry) and dioxus/mobile
    - fixes Android app launch

--- native/src/main.rs
+++ native/src/main.rs
@@ -1,29 +1,5 @@
-use dioxus_native::prelude::*;
 use ui::App;
 
-#[cfg(target_os = "android")]
-#[unsafe(no_mangle)]
-fn android_main(app: android_activity::AndroidApp) {
-
-    android_logger::init_once(
-        android_logger::Config::default().with_max_level(log::LevelFilter::Info),
-    );
-    
-    dioxus_native::launch(App);
-}
-
-#[cfg(target_os = "android")]
-fn main() {}
-
-#[cfg(not(target_os = "android"))]
 fn main() {
-
-    #[cfg(target_os = "linux")]
-    unsafe {
-        std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
-        std::env::set_var("WGPU_BACKEND", "vulkan");
-    }
-
-    dioxus_native::launch(App);
-
+    dioxus::launch(App);
 }
